196f2d541ce58d5953b52a788d06a0c010fe681f
Chelsea Bradbury <chelseabradbury.exe@gmail.com>
2022-09-07 16:55:45 -0700

    Add struct_pack util, parse dos header

---
+++ src/cpputils/struct_pack.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef __GNUC__
+#define PACK(__Declaration__) __Declaration__ __attribute__((__packed__))
+#endif
+
+#ifdef _MSC_VER
+#define PACK(__Declaration__) \
+    __pragma(pack(push, 1)) __Declaration__ __pragma(pack(pop))
+#endif
+
+/* Prevents automatic byte alignment (to four bytes, or whatever) of struct data
+ * Use like this:
+ *
+ * PACK(struct MyStruct {
+ *   uint32_t foo;
+ *   uint32_t bar;
+ * });
+ */
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include "cpputils/file_io.h"
 
+#include "dos_header.h"
+
 int main()
 {
     std::string filepath = "./exe/BF2_r.exe";
@@ -10,6 +12,11 @@ int main()
     char *pFile = loadFileIntoMemory(filepath, length);
 
     printf("length: %i\n", length);
+
+    DosHeader dosHeader;
+    memcpy(&dosHeader, pFile, sizeof(dosHeader));
+
+    printf("dosHeader.e_magic: 0x%04X\n", dosHeader.e_magic);
 
     return 0;
 }
