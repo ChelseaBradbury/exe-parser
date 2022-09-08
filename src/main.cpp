22361fa03dfc8f04524b1bfeb580d594aeb0ec40
Chelsea Bradbury <chelseabradbury.exe@gmail.com>
2022-09-07 17:24:52 -0700

    Add pe header parsing

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "cpputils/file_io.h"
 
 #include "dos_header.h"
+#include "pe_header.h"
 
 int main()
 {
@@ -17,6 +18,20 @@ int main()
     memcpy(&dosHeader, pFile, sizeof(dosHeader));
 
     printf("dosHeader.e_magic: 0x%04X\n", dosHeader.e_magic);
+
+    printf("dosHeader.e_lfanew: 0x%08X\n", dosHeader.e_lfanew);
+
+    PeHeader peHeader;
+    memcpy(&peHeader, pFile + dosHeader.e_lfanew, sizeof(peHeader));
+
+    PeOptionalHeader peOptional;
+    memcpy(&peOptional, pFile + dosHeader.e_lfanew + sizeof(peHeader), sizeof(peOptional));
+
+    printf("image major %i minor %i \n", peOptional.MajorImageVersion, peOptional.MinorImageVersion);
+
+    printf("OS major %i minor %i \n", peOptional.MajorOperatingSystemVersion, peOptional.MinorOperatingSystemVersion);
+
+    printf("linker major %i minor %i \n", peOptional.MajorLinkerVersion, peOptional.MinorLinkerVersion);
 
     return 0;
 }
---
+++ src/pe_header.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "cpputils/struct_pack.h"
+
+PACK(struct PeHeader {
+    uint16_t Machine;
+    uint16_t NumberOfSections;
+    uint32_t TimeDateStamp;
+    uint32_t PointerToSymbolTable;
+    uint32_t NumberOfSymbols;
+    uint16_t SizeOfOptionalHeader;
+    uint16_t Characteristics;
+});
+
+PACK(struct PeOptionalHeader {
+    uint16_t Magic;
+    uint8_t MajorLinkerVersion;
+    uint8_t MinorLinkerVersion;
+    uint32_t SizeOfCode;
+    uint32_t SizeOfInitializedData;
+    uint32_t SizeOfUninitializedData;
+    uint32_t AddressOfEntryPoint;
+    uint32_t BaseOfCode;
+    uint32_t BaseOfData;
+    // NT additional fields.
+    uint32_t ImageBase;
+    uint32_t SectionAlignment;
+    uint32_t FileAlignment;
+    uint16_t MajorOperatingSystemVersion;
+    uint16_t MinorOperatingSystemVersion;
+    uint16_t MajorImageVersion;
+    uint16_t MinorImageVersion;
+    uint16_t MajorSubsystemVersion;
+    uint16_t MinorSubsystemVersion;
+    uint32_t Reserved1;
+    uint32_t SizeOfImage;
+    uint32_t SizeOfHeaders;
+    uint32_t CheckSum;
+    uint16_t Subsystem;
+    uint16_t DllCharacteristics;
+    uint32_t SizeOfStackReserve;
+    uint32_t SizeOfStackCommit;
+    uint32_t SizeOfHeapReserve;
+    uint32_t SizeOfHeapCommit;
+    uint32_t LoaderFlags;
+    uint32_t NumberOfRvaAndSizes;
+    // IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+});
