9086ace249a6a9fbaaaa13e62d5eb19601b16f02
Chelsea Bradbury <chelseabradbury.exe@gmail.com>
2022-09-11 17:03:10 -0700

    Improve header info output, add hex dumping of sections to terminal

--- src/portable_executable/portable_executable.cpp
+++ src/portable_executable/portable_executable.cpp
@@ -1,5 +1,41 @@
 #include "portable_executable.h"
 
+void PrintHex(uint8_t* pData, uint32_t dataLen, uint32_t lineLen) {
+  uint32_t localLen = dataLen + (dataLen % lineLen);
+  uint8_t* pLocal = new uint8_t[localLen];
+  memcpy(pLocal, pData, dataLen);
+
+  uint32_t numLines = localLen / lineLen;
+
+  uint8_t* pLine = new uint8_t[lineLen + 1];
+  pLine[lineLen] = 0x00;
+
+  for (uint32_t i = 0; i < numLines; i++) {
+    auto lineOffset = lineLen * i;
+    for (uint32_t j = 0; j < lineLen; j++) {
+      auto offset = lineOffset + j;
+      printf("%02X", *(pLocal + offset));
+
+      if (j % 2 == 1) {
+        printf(" ");
+      }
+
+      // Copy char to pLine
+      uint8_t chr = *(pLocal + offset);
+      if (chr <= 0x1F) {
+        pLine[j] = '.';
+      } else {
+        pLine[j] = chr;
+      }
+    }
+
+    printf("%s\n", pLine);
+  }
+
+  delete[] pLine;
+  delete[] pLocal;
+}
+
 PortableExecutable::PortableExecutable(uint8_t* pFile) {
   this->m_pFile = pFile;
 
@@ -24,11 +60,13 @@ void PortableExecutable::PrintFileInfo() {
   printf("pe  %p %lu\n", this->m_pPeHeader, sizeof(PeHeader));
   printf("op  %p %lu\n", this->m_pOpHeader, sizeof(PeOptionalHeader));
   printf("sec %p %lu\n", this->m_pSectionHeaders, sizeof(PeSectionHeader));
+  printf("\n");
 
   // DOS header
   printf("dosHeader.e_magic: 0x%04X\n", this->m_pDosHeader->e_magic);
   printf("dosHeader.e_lfanew: 0x%08X\n", this->m_pDosHeader->e_lfanew);
   printf("dosHeader numPages: %i\n", this->m_pDosHeader->e_cp);
+  printf("\n");
 
   // PE header
   printf("peHeader.Magic 0x%08X\n", this->m_pPeHeader->Magic);
@@ -37,6 +75,7 @@ void PortableExecutable::PrintFileInfo() {
   printf("peHeader.NumberOfSymbols %i\n", this->m_pPeHeader->NumberOfSymbols);
   printf("peHeader.SizeOfOptionalHeader %i\n",
          this->m_pPeHeader->SizeOfOptionalHeader);
+  printf("\n");
 
   // PE optional header
   printf("opHeader image ver %i.%i\n", this->m_pOpHeader->MajorImageVersion,
@@ -44,8 +83,12 @@ void PortableExecutable::PrintFileInfo() {
   printf("opHeader OS ver %i.%i\n",
          this->m_pOpHeader->MajorOperatingSystemVersion,
          this->m_pOpHeader->MinorOperatingSystemVersion);
+  printf("opHeader subsys ver %i.%i\n",
+         this->m_pOpHeader->MajorSubsystemVersion,
+         this->m_pOpHeader->MinorSubsystemVersion);
   printf("opHeader linker ver %i.%i\n", this->m_pOpHeader->MajorLinkerVersion,
          this->m_pOpHeader->MinorLinkerVersion);
+  printf("\n");
 }
 
 void PortableExecutable::PrintSectionInfo() {
@@ -53,7 +96,7 @@ void PortableExecutable::PrintSectionInfo() {
   for (int i = 0; i < this->m_pPeHeader->NumberOfSections; i++) {
     auto pSectionHeader = this->m_pSectionHeaders[i];
 
-    printf("section %i name %s\n", i, pSectionHeader.Name);
+    printf("section %i %s\n", i, pSectionHeader.Name);
 
     printf("  VirtualAddress 0x%08X\n", pSectionHeader.VirtualAddress);
     printf("  SizeOfRawData 0x%08X\n", pSectionHeader.SizeOfRawData);
@@ -67,5 +110,11 @@ void PortableExecutable::PrintSectionInfo() {
     printf("  NumberOfLinenumbers 0x%08X\n",
            pSectionHeader.NumberOfLinenumbers);
     printf("  Characteristics 0x%08X\n", pSectionHeader.Characteristics);
+    printf("\n");
+
+    PrintHex(this->m_pFile + pSectionHeader.PointerToRawData,
+             pSectionHeader.SizeOfRawData, 48);
+
+    printf("\n");
   }
 }
